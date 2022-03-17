af38803b3c0f18089539d1f56798741907ea7abf
Alireza-Al <90038814+Alireza-Al@users.noreply.github.com>
2022-03-17 21:49:37 +0330

    creation

---
+++ src/component/App.jsx
@@ -0,0 +1,12 @@
+import styles from '../asset/App.scss';
+// crtl + shift change every line place you like 
+
+function App() {
+  return (
+    <div className="App">
+      New Twitter
+    </div>
+  );
+}
+
+export default App;

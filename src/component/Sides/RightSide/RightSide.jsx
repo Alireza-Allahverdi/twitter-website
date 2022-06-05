071bfe98835cad8b23311902b5a51b8e846451ed
Alireza-Al <90038814+Alireza-Al@users.noreply.github.com>
2022-06-05 16:23:05 +0430

    right sidebar links set

--- src/component/Sides/RightSide/RightSide.jsx
+++ src/component/Sides/RightSide/RightSide.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTwitter } from '@fortawesome/free-brands-svg-icons'
 import { faHashtag } from '@fortawesome/free-solid-svg-icons';
@@ -6,36 +6,49 @@ import { faHashtag } from '@fortawesome/free-solid-svg-icons';
 
 function RightSide() {
 
+    const eeeeee = [
+        'پرچم_دار_جدید',
+        'پرچم_دار_قدیم',
+        'پرچم_دار_آینده',
+
+    ]
+
     return (
         <div className='rightSide'>
-
-            <div className="twitterTitle">
-                <span className='icon'>
-                    <FontAwesomeIcon icon={faTwitter} />
-                </span>
-                <span className='twitterFarsi'>
-                    توییتر فارسی
-                </span>
-            </div>
+            <a href="/">
+                <div className="twitterTitle">
+                    <span className='icon'>
+                        <FontAwesomeIcon icon={faTwitter} />
+                    </span>
+                    <span className='twitterFarsi'>
+                        توییتر فارسی
+                    </span>
+                </div>
+            </a>
 
             <h2 className='hashtagTittle'>
                 داغ ترین هشتگ ها
             </h2>
 
-            {/*thsi is where the mapping begin :)))*/}
-            <a href="#">
-                <button className='hashtagButton'>
-                    <div className="hashtagContainer">
-                        <span className='hashtagIcon'>
-                            <FontAwesomeIcon className='hashtagSelf' icon={faHashtag} />
-                        </span>
-                        <span className='hashtagSubject'>
-                            پرچم_دار_جدید
-                        </span>
-                    </div>
-                </button>
-
-            </a>
+            {/*this is where the mapping begin :)))*/}
+            {
+                eeeeee.map((item, index) => {
+                    return <Fragment key={index}>
+                        <a href={`/hashtags/${item}`}>
+                            <button className='hashtagButton'>
+                                <div className="hashtagContainer">
+                                    <span className='hashtagIcon'>
+                                        <FontAwesomeIcon className='hashtagSelf' icon={faHashtag} />
+                                    </span>
+                                    <span className='hashtagSubject'>
+                                        {item}
+                                    </span>
+                                </div>
+                            </button>
+                        </a>
+                    </Fragment>
+                })
+            }
         </div>
     )
 }

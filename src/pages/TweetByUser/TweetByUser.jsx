1dc8b5167a0351a210809e172568ffae81edf6d0
Alireza-Al <90038814+Alireza-Al@users.noreply.github.com>
2022-06-05 16:32:37 +0430

    tweet by user commenting

--- src/pages/TweetByUser/TweetByUser.jsx
+++ src/pages/TweetByUser/TweetByUser.jsx
@@ -7,11 +7,12 @@ import { useParams } from 'react-router';
 
 function TweetByUser() {
 
-    const {user} = useParams();
+    const { user } = useParams(); // will give access to params of the route path ( mu know and include the params name in this example it is user )
+    // const param = useParams() // if this one is used most include the params name aswell for example in this project it is param.user
 
     return (
-        <div className='mainSide'>
-            <Header tittle={user} icon={<FontAwesomeIcon icon={faUser} />} />
+        <div className='mainSide'> {/* this class must be given to the page so that it will have the same styles */}
+            <Header tittle={user} icon={<FontAwesomeIcon icon={faUser} />} /> {/* header that will get the icon and tittle as the props and displays them */}
             <PostsList />
         </div>
     )

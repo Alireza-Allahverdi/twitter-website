1de32ad90cbc44cecf60fbe18dc9fd24ff17b6ea
Alireza-Al <90038814+Alireza-Al@users.noreply.github.com>
2022-06-06 15:00:31 +0430

    new tweet text area onChange handled

--- src/pages/HomePage/NewTweet/NewTweetSlot.jsx
+++ src/pages/HomePage/NewTweet/NewTweetSlot.jsx
@@ -1,8 +1,13 @@
+import React, { useEffect, useState } from 'react'
 import { faImage } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React from 'react'
 
 function NewTweetSlot() {
+
+    const [tweetField, setTweetField] = useState({
+        tweet: ''
+    })
+
     return (
         <div className='newTweet'>
             <div className="tweetContainer">
@@ -10,14 +15,23 @@ function NewTweetSlot() {
                 <span>
                     <FontAwesomeIcon icon={faImage} />
                 </span>
-                <textarea name="tweetPlease" id="" placeholder='توییت کن...' className='tweetUserTextArea' />
+                <textarea
+                    name="tweetPlease"
+                    id="tweetSlot"
+                    placeholder='توییت کن...'
+                    className='tweetUserTextArea'
+                    onChange={(e) => {
+                        setTweetField({
+                            tweet: e.target.value
+                        })
+                    }} />
             </div>
             <div className="tweetButtonsContainer">
                 <button className='TweetButton'>
                     توییت
                 </button>
                 <button className="imgTweetButton">
-                    <FontAwesomeIcon icon={faImage} className='imgTweetIcon'/>
+                    <FontAwesomeIcon icon={faImage} className='imgTweetIcon' />
                 </button>
             </div>
         </div>

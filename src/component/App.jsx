994b8146b75cc35019bb716ec3b90ddc90093228
Alireza-Al <90038814+Alireza-Al@users.noreply.github.com>
2022-06-07 18:17:12 +0430

    sidebarlayout children set and auth route made

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -1,31 +1,29 @@
-import RightSide from './Sides/RightSide/RightSide';
 import Home from '../pages/HomePage/Home';
-import LeftSide from './Sides/LeftSide/LeftSide';
 import '../asset/App.scss';
 import TweetByHashTag from '../pages/TweetsByHashTag/TweetByHashTag';
 import TweetByUser from '../pages/TweetByUser/TweetByUser';
 import { Route, Routes } from 'react-router-dom';
 import Error404 from '../pages/P404/Page404';
+import SideBarLayout from './Sides/Layout/SideBarLayout';
+import Auth from '../pages/Auth/Auth';
+
 
 function App() {
 
-
     return (
-
         <div className="app">
-            <RightSide />
-            <hr />
             <Routes>
-                <Route path={'/'} element={<Home />} />
-                <Route path={'hashtags/:hashtag'} element={<TweetByHashTag />} />
-                <Route path={'users/:user'} element={<TweetByUser />} />
-                <Route path={'*'} element={<Error404 />} />
+                <Route element={<SideBarLayout />}>
+                    <Route path={'/'} element={<Home />} />
+                    <Route path={'hashtags/:hashtag'} element={<TweetByHashTag />} />
+                    <Route path={'users/:user'} element={<TweetByUser />} />
+                    <Route path={'*'} element={<Error404 />} />
+                </Route>
+                <Route path={'auth'} element={<Auth />} />
             </Routes>
 
             {/* <TweetByHashTag /> */}
             {/* <TweetByUser /> */}
-            <hr />
-            <LeftSide />
         </div>
     );
 }

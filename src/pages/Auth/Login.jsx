a6cfe1a23cb3af72bd4337ee8c27adb4cd2f562d
Alireza-Al <90038814+Alireza-Al@users.noreply.github.com>
2022-06-11 13:29:51 +0430

    login solid without state and styles made

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 
 function Login() {
-  return (
-    <div>Login</div>
-  )
+    return (
+        <div className="login">
+            {/* TODO complete state management for this file */}
+            <div className="userNameInp">
+                <label htmlFor="username">
+                    نام کاربری
+                </label>
+                <input
+                    type="text"
+                />
+            </div>
+            <div className="passwordInp">
+                <label htmlFor="password">
+                    کلمه عبور
+                </label>
+                <input
+                    type="password"
+                />
+            </div>
+        </div>
+    )
 }
 
 export default Login

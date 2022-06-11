82453d190344c6beb251d4f6d9181ca1f044f1fe
Alireza-Al <90038814+Alireza-Al@users.noreply.github.com>
2022-06-11 22:10:41 +0430

    register static with styles made

--- src/pages/Auth/Register.jsx
+++ src/pages/Auth/Register.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
 
 function Register() {
-  return (
-    <div>Register</div>
-  )
+    return (
+        <div className='form'>
+            {/* TODO complete state management for this file */}
+            <label htmlFor="fullname">
+                نام کامل
+            </label>
+            <input
+                type="text"
+            />
+            <label htmlFor="username">
+                نام کاربری
+            </label>
+            <input
+                type="text"
+            />
+            <label htmlFor="password">
+                کلمه عبور
+            </label>
+            <input
+                type="password"
+            />
+            <label htmlFor="repeat-password">
+                تکرار کلمه عبور
+            </label>
+            <input
+                type="password"
+            />
+            <button className="registerBtn">
+                ثبت نام
+            </button>
+        </div>
+    )
 }
 
 export default Register

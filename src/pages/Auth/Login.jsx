842bd09e7f3a18e5dbf1f4522e39870c961585fa
Alireza-Al <90038814+Alireza-Al@users.noreply.github.com>
2022-07-01 18:20:42 +0430

    login input validation, input onchange handled

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,22 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { validateRegexPassword } from './Regex'
 
 function Login() {
+
+    const [field, setField] = useState({
+        username: '',
+        password: ''
+    })
+
+    const ValidateAndAccept = () => {
+        if (!field.username && !field.password) return alert('fileds empty')
+        return Loginer()
+    }
+
+    const Loginer = () => {
+        alert('all set')
+    }
+
     return (
         <div className="form">
             {/* TODO complete state management for this file */}
-                <label htmlFor="username">
-                    نام کاربری
-                </label>
-                <input
-                    type="text"
-                />
-                <label htmlFor="password">
-                    کلمه عبور
-                </label>
-                <input
-                    type="password"
-                />
-            <button className="loginBtn">
+            <label htmlFor="username">
+                نام کاربری
+            </label>
+            <input
+                type="text"
+                onChange={(e) => {
+                    setField({
+                        ...field,
+                        username: e.target.value
+                    })
+                }}
+            />
+            <label htmlFor="password">
+                کلمه عبور
+            </label>
+            <input
+                type="password"
+                onChange={(e) => {
+                    setField({
+                        ...field,
+                        password: e.target.value
+                    })
+                }}
+            />
+            <button className="loginBtn" onClick={ValidateAndAccept}>
                 ورود
             </button>
         </div>

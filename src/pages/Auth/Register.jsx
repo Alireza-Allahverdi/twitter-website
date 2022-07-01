542538341ffd8ba386971200a5ed0089f1a68723
Alireza-Al <90038814+Alireza-Al@users.noreply.github.com>
2022-07-01 18:19:49 +0430

    register regexp, input vaidation, input onchange handled

--- src/pages/Auth/Register.jsx
+++ src/pages/Auth/Register.jsx
@@ -1,6 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { validateRegexPassword } from './Regex'
 
 function Register() {
+
+    const REGEX_PASSWORD = "^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[@$!%*#?&])[A-Za-z0-9@$!%*#?&]{8,}$";
+
+    const [state, setState] = useState({
+        name: '',
+        userName: '',
+        password:'',
+        rePassword:''
+    })
+
+    const ValidateAndAccept = () => {
+        if (!state.name || !state.userName || !state.password) {
+            return alert("یکی از فیلد ها خالی است")
+        }
+        if (!validateRegexPassword.test(state.password)) {
+            return alert("الگوی رمز عبور اشتباه است")
+        }
+        return Logger()
+    }
+
+    const Logger = () => {
+        alert('logged')
+    }
+
+    useEffect(() => {
+        console.log(state);
+    }, [state])
+
     return (
         <div className='form'>
             {/* TODO complete state management for this file */}
@@ -9,26 +38,54 @@ function Register() {
             </label>
             <input
                 type="text"
+                onChange={e => setState({
+                    ...state,
+                    name: e.target.value
+                })}
             />
             <label htmlFor="username">
                 نام کاربری
             </label>
             <input
                 type="text"
+                onChange={e => setState({
+                    ...state,
+                    userName: e.target.value
+                })}
             />
             <label htmlFor="password">
                 کلمه عبور
             </label>
+            <p>
+                (کلمه عبور باید شامل حداقل 8 حرف، یک عدد و شامل یکی از حروف !@#$%^&* باشد)
+            </p>
             <input
                 type="password"
+                pattern={REGEX_PASSWORD}
+                onChange={e => setState({
+                    ...state,
+                    password: e.target.value
+                })}
             />
             <label htmlFor="repeat-password">
                 تکرار کلمه عبور
             </label>
             <input
                 type="password"
+                onChange={e => setState({
+                    ...state,
+                    rePassword: e.target.value
+                })}
             />
-            <button className="registerBtn">
+            {
+                state.password !== state.rePassword
+                    ?
+                    <span className='errHint'>
+                        تکرار رمز باید یکی باشد
+                    </span>
+                    : ''
+            }
+            <button className="registerBtn" onClick={ValidateAndAccept}>
                 ثبت نام
             </button>
         </div>

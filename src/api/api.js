cc8c7ca1fd49cf53991d029d28b654858d5e1d4a
Alireza-Al <90038814+Alireza-Al@users.noreply.github.com>
2022-07-01 17:12:22 +0430

    create api base url

---
+++ src/api/api.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export default axios.create({
+    baseURL: "https://twitterapi.liara.run/", // create a base url so that it wont be needed to write url eachtime requesting to server
+    // headers: { // when there is discussion about security of website we would need header to authorize our requests to server
+    //     API_KEY: "lsdkljfalksfjasdfkjlasfjklasdkfjsadjf"
+    // }
+})

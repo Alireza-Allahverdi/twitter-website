cebae41c1831ce483a736918fbe3dd952d4c41e7
Alireza-Al <90038814+Alireza-Al@users.noreply.github.com>
2022-07-19 01:08:03 +0430

    if the user is logged will always redirect to home else always to auth

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -1,8 +1,8 @@
-import Home from '../pages/HomePage/Home';
 import '../asset/App.scss';
+import Home from '../pages/HomePage/Home';
 import TweetByHashTag from '../pages/TweetsByHashTag/TweetByHashTag';
 import TweetByUser from '../pages/TweetByUser/TweetByUser';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import Error404 from '../pages/P404/Page404';
 import SideBarLayout from './Sides/Layout/SideBarLayout';
 import Auth from '../pages/Auth/Auth';
@@ -13,13 +13,13 @@ function App() {
     return (
         <div className="app">
             <Routes>
-                <Route element={<SideBarLayout />}>
+                <Route element={isLoggedIn() ? <SideBarLayout /> : <Auth/>}>
                     <Route path={'/'} element={<Home />} />
                     <Route path={'hashtags/:hashtag'} element={<TweetByHashTag />} />
                     <Route path={'users/:user'} element={<TweetByUser />} />
                     <Route path={'*'} element={<Error404 />} />
                 </Route>
-                <Route path={'auth'} element={<Auth />} />
+                <Route path={'auth'} element={isLoggedIn() ? <Navigate to={"/"}/> : <Auth />} />
             </Routes>
 
             {/* <TweetByHashTag /> */}
@@ -27,5 +27,8 @@ function App() {
         </div>
     );
 }
+
+const isLoggedIn = () => !!localStorage.getItem("x-auth-token")
+
 
 export default App;

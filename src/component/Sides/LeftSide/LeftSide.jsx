acea114cb14ec94e016d74404c4c2cf55299fb2d
Alireza-Al <90038814+Alireza-Al@users.noreply.github.com>
2022-07-30 04:12:45 +0430

    users data fetch and loaders added

--- src/component/Sides/LeftSide/LeftSide.jsx
+++ src/component/Sides/LeftSide/LeftSide.jsx
@@ -1,152 +1,179 @@
-import React, { useEffect, useRef, useState } from 'react'
-import { faHashtag, faUser } from '@fortawesome/free-solid-svg-icons';
+import React, { Fragment, useEffect, useRef, useState } from 'react'
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { UploadUserPhoto } from '../../../api/auth-api';
+import { GetUsers } from '../../../api/user-api';
 
 function LeftSide() {
 
-    const [dropdownState, setDropdownState] = useState(false)
-    const [userInfo, setUserInfo] = useState({
-        name: !!localStorage.getItem('name') ? localStorage.getItem('name') : "no data",
-        userName: !!localStorage.getItem('username') ? localStorage.getItem('username') : "no data",
-        image: localStorage.getItem('image')
-    })
-    const [imageFile, setImageFile] = useState()
-    const [loader, setLoader] = useState(false)
+   const [users, setUsers] = useState([])
+   const [dropdownState, setDropdownState] = useState(false)
+   const [userInfo, setUserInfo] = useState({
+      name: !!localStorage.getItem('name') ? localStorage.getItem('name') : "no data",
+      userName: !!localStorage.getItem('username') ? localStorage.getItem('username') : "no data",
+      image: localStorage.getItem('image')
+   })
+   const [imageFile, setImageFile] = useState()
+   const [loaderImg, setLoaderImg] = useState(false)
+   const [loaderUser, setLoaderUser] = useState(false)
 
-    const imageInp = useRef()
+   const imageInp = useRef()
 
-    const handleImage = (e) => {
-        if (e.target.files && e.target.files.length > 0) {
-            setImageFile(e.target.files[0])
-        }
+   const handleImage = (e) => {
+      if (e.target.files && e.target.files.length > 0) {
+         setImageFile(e.target.files[0])
+      }
+      const fileReader = new FileReader()
+      fileReader.onload = (e) => {
+         setUserInfo({
+            ...userInfo,
+            image: e.target.result
+         })
+      }
+      fileReader.readAsDataURL(e.target.files[0])
 
-        const fileReader = new FileReader()
-        fileReader.onload = (e) => {
-            setUserInfo({
-                ...userInfo,
-                image: e.target.result
-            })
-        }
-        fileReader.readAsDataURL(e.target.files[0])
+      const formData = new FormData()
+      formData.append("image", e.target.files[0])
+      setLoaderImg(true)
+      UploadUserPhoto(formData, (isOk, data) => {
+         if (!isOk) {
+            return alert(data)
+         }
+         alert('عکس پروفایل تغییر کرد')
+         setLoaderImg(false)
+         localStorage.setItem("image", data.imagePath)
+      })
+   }
 
-        const formData = new FormData()
-        formData.append("image", e.target.files[0])
-        setLoader(true)
-        UploadUserPhoto(formData, (isOk, data) => {
-            if (!isOk) {
-                return alert(data)
-            }
-            alert('عکس پروفایل تغییر کرد')
-            setLoader(false)
-            localStorage.setItem("image", data.imagePath)
-        })
-    }
-    useEffect(() => {
-        console.log(userInfo);
-    }, [userInfo])
+   useEffect(() => {
+      setLoaderUser(true)
+      GetUsers((isOk, data) => {
+         if (!isOk) {
+            return
+         }
+         setLoaderUser(false)
+         setUsers(data)
+      })
+   }, [])
 
-    return (
-        <div className='leftSide'>
-            <div
-                className="userInfo"
-                onClick={() => {
-                    setDropdownState(!dropdownState)
-                }}
-            >
-                {
-                    loader ?
-                    <div className="spinner-container">
-                        <div className="spinner"></div>
-                    </div>
-                    :
-                    userInfo.image ||
-                    userInfo.image !== "undefined" ?
-                    <img src={userInfo.image} alt="" />
-                    : <FontAwesomeIcon icon={faUser} />
-
-                }
-                <div className="profileText">
-                    <div className='persianUserName'>
-                        {userInfo.name}
-                    </div>
-                    <div className='userID'>
-                        {userInfo.userName}
-                    </div>
-                </div>
-                <input
-                    ref={imageInp}
-                    type="file"
-                    style={{ display: "none" }}
-                    onChange={handleImage}
-                />
-            </div>
+   return (
+      <div className='leftSide'>
+         <div
+            className="userInfo"
+            onClick={() => {
+               setDropdownState(!dropdownState)
+            }}
+         >
             {
-                dropdownState &&
-                <div
-                    className="dropdownMenu"
-                    onMouseLeave={() => { setDropdownState(false) }}
-                >
-                    {/* TODO complete progfile page */}
-                    <div className="dropdownItem">
-                        <p>
-                            پروفایل
-                        </p>
-                    </div>
-                    <div
-                        className="dropdownItem"
-                        onClick={() => {
-                            imageInp.current.click()
-                        }}
-                    >
-                        <p>
-                            آپلود عکس پروفایل
-                        </p>
-                    </div>
-                    <div
-                        className="dropdownItem"
-                        onClick={() => {
-                            localStorage.clear()
-                            window.location.reload()
-                        }}
-                    >
-                        <p>
-                            خروج
-                        </p>
-                    </div>
-                </div>
+               loaderImg ?
+                  <div className="spinner-container">
+                     <div className="spinner"></div>
+                  </div>
+                  :
+                  userInfo.image ||
+                     userInfo.image !== "undefined" ?
+                     <img src={userInfo.image} alt="" />
+                     : <FontAwesomeIcon icon={faUser} />
 
             }
-
-            <div className="bestTwittersContainer">
-                <h3 className='bestTwitterTittle'>
-                    بهترین توییت کنندگان
-                </h3>
-                <div className="divider"></div>
-
-                <a href="/users/khodee">
-                    <button className='bestTwittersButton'>
-                        <div className="twitters">
-                            {/* <img className='bestTwittersImg' src="" alt="" /> */}
-                            <span className='bestTwittersImg'>
-                                <FontAwesomeIcon icon={faHashtag} />
-                            </span>
-                            <div className="twitterProfileText">
-                                <div className='twitterPersianName'>
-                                    علیرضا الهوردی
-                                </div>
-                                <div className='twitterID'>
-                                    AlirezaAllahverdi20
-                                </div>
-                            </div>
-                        </div>
-                    </button>
-                </a>
-                <div className="twittersDivider"></div>
-
+            <div className="profileText">
+               <div className='persianUserName'>
+                  {userInfo.name}
+               </div>
+               <div className='userID'>
+                  {userInfo.userName}
+               </div>
             </div>
-        </div>
-    )
+            <input
+               ref={imageInp}
+               type="file"
+               style={{ display: "none" }}
+               onChange={handleImage}
+            />
+         </div>
+         {
+            dropdownState &&
+            <div
+               className="dropdownMenu"
+               onMouseLeave={() => { setDropdownState(false) }}
+            >
+               {/* TODO complete progfile page */}
+               <div className="dropdownItem">
+                  <p>
+                     پروفایل
+                  </p>
+               </div>
+               <div
+                  className="dropdownItem"
+                  onClick={() => {
+                     imageInp.current.click()
+                  }}
+               >
+                  <p>
+                     آپلود عکس پروفایل
+                  </p>
+               </div>
+               <div
+                  className="dropdownItem"
+                  onClick={() => {
+                     localStorage.clear()
+                     window.location.reload()
+                  }}
+               >
+                  <p>
+                     خروج
+                  </p>
+               </div>
+            </div>
+
+         }
+
+         <div className="bestTwittersContainer">
+            <h3 className='bestTwitterTittle'>
+               بهترین توییت کنندگان
+            </h3>
+            <div className="divider"></div>
+            {
+               loaderUser ?
+                  <div className="spinner-container">
+                     <div className="spinner"></div>
+                  </div>
+                  :
+                  <>
+                     {
+                        users.map((item, index) => {
+                           return <Fragment key={index}>
+                              <a href={`/users/${item.username}`}>
+                                 <button className='bestTwittersButton'>
+                                    <div className="twitters">
+                                       <span className='bestTwittersImg'>
+                                          {
+                                             item.image ?
+                                                <img className='bestTwittersImg' src={item.image} alt="" />
+                                                : <FontAwesomeIcon icon={faUser} />
+
+                                          }
+                                       </span>
+                                       <div className="twitterProfileText">
+                                          <div className='twitterPersianName'>
+                                             {item.name}
+                                          </div>
+                                          <div className='twitterID'>
+                                             {item.username}
+                                          </div>
+                                       </div>
+                                    </div>
+                                 </button>
+                              </a>
+                              <div className="twittersDivider"></div>
+                           </Fragment>
+                        })
+                     }
+                  </>
+            }
+         </div>
+      </div>
+   )
 }
 
 export default LeftSide

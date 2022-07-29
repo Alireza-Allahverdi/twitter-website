ae7ca637a5d4f6e08d09d91ebf812d2e39e1d8e0
Alireza-Al <90038814+Alireza-Al@users.noreply.github.com>
2022-07-29 17:14:21 +0430

    home getall tweet info handled to be passed with props to other files

--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -1,20 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../../component/Header/Header';
 import NewTweetSlot from './NewTweet/NewTweetSlot';
 import PostsList from '../../component/Posts/PostsList';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { GetTweets } from '../../api/tweet-api';
 
 
 function Home() {
 
-  return (
-    <div className='mainSide'> 
-      <Header tittle={'خانه'} icon={<FontAwesomeIcon icon={faHome}/>}/>
-      <NewTweetSlot />
-      <PostsList />
-    </div>
-  )
+    const [tweets, setTweets] = useState([])
+
+    useEffect(() => {
+        GetTweets((isOk, data) => {
+            if (!isOk) {
+                return alert(data)
+            }
+            setTweets(data)
+        })
+    }, [])
+
+    useEffect(() => {
+        console.log(tweets);
+    }, [tweets])
+
+    return (
+        <div className='mainSide'>
+            <Header tittle={'خانه'} icon={<FontAwesomeIcon icon={faHome} />} />
+            <NewTweetSlot />
+            <PostsList data={tweets}/>
+        </div>
+    )
 }
 
 export default Home;

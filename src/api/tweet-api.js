5cf0d42f293967bf95e7cae92d294ec27efb1fb8
Alireza-Al <90038814+Alireza-Al@users.noreply.github.com>
2022-07-31 21:49:14 +0430

    like tweet api instance set

--- src/api/tweet-api.js
+++ src/api/tweet-api.js
@@ -1,5 +1,6 @@
 import { AxiosPrivate } from "./api";
 
+// will get all the tweets from server
 export const GetTweets = (callback) => {
     AxiosPrivate()
     .post("getAllTweet")
@@ -13,9 +14,24 @@ export const GetTweets = (callback) => {
     })
 }
 
+// this will send the tweet from newtweet slot to server and meanwhile will be gotten from the postlist
 export const SendTweet = (data, callback) => {
     AxiosPrivate()
     .post("newTweet",data)
+    .then((res) => {
+        let data = res.data
+        callback(true, data)
+    })
+    .catch((err) => {
+        callback(false, err)
+        console.error(err)
+    })
+}
+
+// like tweets request
+export const LikeReq = (id, callback) => {
+    AxiosPrivate()
+    .get(`likeTweet/${id}`)
     .then((res) => {
         let data = res.data
         callback(true, data)

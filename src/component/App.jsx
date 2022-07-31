7a4367b1a26e6b346e8f088bfabccab077407ff7
Alireza-Al <90038814+Alireza-Al@users.noreply.github.com>
2022-07-31 13:41:53 +0430

    tweet provider is wrapped around all components

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -6,21 +6,24 @@ import { Route, Routes, Navigate } from 'react-router-dom';
 import Error404 from '../pages/P404/Page404';
 import SideBarLayout from './Sides/Layout/SideBarLayout';
 import Auth from '../pages/Auth/Auth';
+import { TweetProvider } from '../context/TweetContext';
 
 
 function App() {
 
     return (
         <div className="app">
-            <Routes>
-                <Route element={isLoggedIn() ? <SideBarLayout /> : <Auth/>}>
-                    <Route path={'/'} element={<Home />} />
-                    <Route path={'hashtags/:hashtag'} element={<TweetByHashTag />} />
-                    <Route path={'users/:user'} element={<TweetByUser />} />
-                    <Route path={'*'} element={<Error404 />} />
-                </Route>
-                <Route path={'auth'} element={isLoggedIn() ? <Navigate to={"/"}/> : <Auth />} />
-            </Routes>
+            <TweetProvider>
+                <Routes>
+                    <Route element={isLoggedIn() ? <SideBarLayout /> : <Auth />}>
+                        <Route path={'/'} element={<Home />} />
+                        <Route path={'hashtags/:hashtag'} element={<TweetByHashTag />} />
+                        <Route path={'users/:user'} element={<TweetByUser />} />
+                        <Route path={'*'} element={<Error404 />} />
+                    </Route>
+                    <Route path={'auth'} element={isLoggedIn() ? <Navigate to={"/"} /> : <Auth />} />
+                </Routes>
+            </TweetProvider>
 
             {/* <TweetByHashTag /> */}
             {/* <TweetByUser /> */}

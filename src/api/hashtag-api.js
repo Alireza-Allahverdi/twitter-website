57e7521fc4eb1be762ef2178970c3c21042ad43d
Alireza-Al <90038814+Alireza-Al@users.noreply.github.com>
2022-08-02 15:23:17 +0430

    created a get tweets by hashtag api req

--- src/api/hashtag-api.js
+++ src/api/hashtag-api.js
@@ -5,6 +5,23 @@ export const GetHashtags = (callback) => {
         .get("getAllHashTags")
         .then((res) => {
             let data = res.data
+            console.log(data);
+
+
+            callback(true, data)
+        })
+        .catch((err) => {
+            callback(false, err)
+            console.error(err)
+        })
+}
+
+export const GetTweetsByHashtagReq = (hashTag, callback) => {
+    // we can use destructing here aswell like only writing {hashTag} but its better thus way in my opinion
+    AxiosPrivate()
+        .post("getAllTweet", { hashTag })
+        .then((res) => {
+            let data = res.data
             callback(true, data)
         })
         .catch((err) => {

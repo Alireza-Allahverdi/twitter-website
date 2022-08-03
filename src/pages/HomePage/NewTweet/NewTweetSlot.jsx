366f1dc4665dc423162560615caf23b36676c105
Alireza-Al <90038814+Alireza-Al@users.noreply.github.com>
2022-08-03 13:52:10 +0430

    loader+ update hashtag function added

--- src/pages/HomePage/NewTweet/NewTweetSlot.jsx
+++ src/pages/HomePage/NewTweet/NewTweetSlot.jsx
@@ -1,16 +1,17 @@
-import React, { useRef, useState } from 'react'
+import React, { Fragment, useRef, useState } from 'react'
 import { faImage } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { SendTweet } from '../../../api/tweet-api'
-import { setTweetText, useTweetDispatch, useTweetState } from '../../../context/TweetContext'
+import { setTweetText, updateHashtagList, useTweetDispatch, useTweetState } from '../../../context/TweetContext'
 
 function NewTweetSlot() {
 
     // const [tweetField, setTweetField] = useState()
-    const {tweetText} = useTweetState()
+    const { tweetText } = useTweetState()
     const dispatchTweet = useTweetDispatch()
     const [imageFile, setImageFile] = useState()
     const [imagePath, setImagePath] = useState()
+    const [loader, setLoader] = useState()
 
     const uploadImg = useRef()
 
@@ -38,61 +39,78 @@ function NewTweetSlot() {
             if (!isOk) {
                 return alert(data)
             }
+            if (tweetText.includes("#")) {
+                updateHashtagList(dispatchTweet)
+            }
+            setLoader(false)
             window.location.reload()
         })
     }
 
     return (
         <div className='newTweet'>
-            <div className="tweetContainer">
-                <span>
-                    {
-                        localStorage.getItem("image") ?
-                            <img src={localStorage.getItem("image")} alt="" className='tweetUserImg' />
-                            :
-                            <FontAwesomeIcon icon={faImage} />
-                    }
-                </span>
-                <textarea
-                    name="tweet"
-                    id="tweetSlot"
-                    placeholder='توییت کن...'
-                    className='tweetUserTextArea'
-                    value={tweetText}
-                    onChange={(e) => {
-                        setTweetText(dispatchTweet, e.target.value)
-                    }}
-                />
-                <div className='imageUpload'>
-                    {
-                        imagePath && <img src={imagePath} alt="" />
-                    }
-                </div>
-            </div>
-            <div className="tweetButtonsContainer">
-                <button
-                    className='TweetButton'
-                    onClick={() => {
-                        PostText()
-                    }}
-                >
-                    توییت
-                </button>
-                <button
-                    className="imgTweetButton"
-                    onClick={() => {
-                        uploadImg.current.click()
-                    }}
-                >
-                    <FontAwesomeIcon icon={faImage} className='imgTweetIcon' />
-                </button>
-                <input
-                    type="file"
-                    ref={uploadImg}
-                    onChange={handleImg}
-                    style={{ display: "none" }}
-                />
-            </div>
+            {
+                loader ?
+                    <div className="spinner-container">
+                        <div className="spinner"></div>
+                    </div>
+                    :
+                    <Fragment>
+                        <div className="tweetContainer">
+                            <span>
+                                {
+                                    localStorage.getItem("image") ?
+                                        <img src={localStorage.getItem("image")} alt="" className='tweetUserImg' />
+                                        :
+                                        <FontAwesomeIcon icon={faImage} />
+                                }
+                            </span>
+                            <textarea
+                                name="tweet"
+                                id="tweetSlot"
+                                placeholder='توییت کن...'
+                                className='tweetUserTextArea'
+                                value={tweetText}
+                                onChange={(e) => {
+                                    setTweetText(dispatchTweet, e.target.value)
+                                }}
+                            />
+                            <div className='imageUpload'>
+                                {
+                                    imagePath && <img src={imagePath} alt="" />
+                                }
+                            </div>
+                        </div>
+                        <div className="tweetButtonsContainer">
+                            <button
+                                className='TweetButton'
+                                onClick={() => {
+                                    if (!tweetText) {
+                                        return
+                                    }
+                                    setLoader(true)
+                                    PostText()
+                                }}
+                            >
+                                توییت
+                            </button>
+                            <button
+                                className="imgTweetButton"
+                                onClick={() => {
+                                    uploadImg.current.click()
+                                }}
+                            >
+                                <FontAwesomeIcon icon={faImage} className='imgTweetIcon' />
+                            </button>
+                            <input
+                                type="file"
+                                ref={uploadImg}
+                                onChange={handleImg}
+                                style={{ display: "none" }}
+                            />
+                        </div>
+                    </Fragment>
+            }
         </div>
     )
 }

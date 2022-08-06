dc221954450ae3142afc95b6724d24ef4ade23d4
Alireza-Al <90038814+Alireza-Al@users.noreply.github.com>
2022-08-06 14:23:38 +0430

    tweet provider wraps around index

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './component/App';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { TweetProvider } from './context/TweetContext';
 
 
 ReactDOM.render(
     <React.StrictMode>
-        <Router>
-            <App />
-        </Router>
+        <TweetProvider>
+            <Router>
+                <App />
+            </Router>
+        </TweetProvider>
     </React.StrictMode>,
     document.getElementById('root')
 );

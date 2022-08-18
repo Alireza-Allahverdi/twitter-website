51ae4bb117333ed5118b88356369738dd0a4dc6d
Alireza-Al <90038814+Alireza-Al@users.noreply.github.com>
2022-08-18 14:20:08 +0430

    offcanvas menu added

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { setOffcanvasState, useTweetDispatch } from '../../context/TweetContext'
 
 function Header({ tittle, icon }) {
+
+    const dispatchOffcanvas = useTweetDispatch()
+
     return (
         <>
             <div className='header'>
                 {/* header icon that can be get from props */}
+                <span className="hamburger" onClick={() => {
+                    setOffcanvasState(dispatchOffcanvas)
+                }}>
+                    <FontAwesomeIcon icon={faBars} />
+                </span>
                 <span className="headerIcon">
                     {icon}
                 </span>
